c67494f5692ec833f1bf2759fe27bfc12975efde
Sandra Fernandes <sandrapatfer@gmail.com>
2011-10-12 00:56:35 +0100

    update

--- src/Final-Work/Scripts/template.js
+++ src/Final-Work/Scripts/template.js
@@ -2,9 +2,21 @@
     var document = window.document;
     function template(templateName, templateObject) {
         var templateText = $("script[type='html/template']#" + templateName).text();
-        var regex = /\$\([^\$]*\)/m;
-        var templateParts = regex.exec(templateText);
+        var regex = /\$\([^\)]+\)/g;
+        var templateParts = templateText.match(regex);
+        for (var i = 0; i < templateParts.length; i++) {
+            var replaceText = templateParts[i];
+            var prop = replaceText.substring(2, replaceText.length - 1);
+            templateText = templateText.replace(replaceText, templateObject[prop]);
+        }
+        return templateText;
     }
+    /*
+    1- o 2º param pode ser um array - como ver o tipo do param?
+    2- a função apenas tem um param e deve devolver uma função, estranho...
+    3- as marcas serem objectos dentro de objectos - como é q se identificam?
+    */
 
     window.template = template;
 })(window);
+
